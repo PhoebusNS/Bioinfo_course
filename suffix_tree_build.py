a4386658e966d1cdbb91b87c914db0b1d2e30d75
JacquesFGD <jacques.deguine@gmail.com>
2015-03-29 10:53:09 -0700

    Suffix tree builder, but reduction of non branching edges not implemented

--- suffix_tree_build.py
+++ suffix_tree_build.py
@@ -31,21 +31,60 @@ def update_edges(index, node):
     letter = genome[index]
     for i in node['_e']:
         if genome[i] == letter:
+            node['_e'].remove(i)
+            if not node['_e']:
+                del node['_e']
             node[letter] = {'_e': [i+1]}
             update_edges(index+1, node[letter])
             return None
     node['_e'].append(index)
     return None
 
+def edge_len(node):
+    l = 0
+    for key, value in node.items():
+        if key == '_e':
+            l += len(node['_e'])
+        else:
+            l += 1
+    return l
 
-def tree_reducer(tree):
+
+def tree_reducer(root_node):
     """Tree_reducer
-    Reduces all non branching paths to multiple letter paths"""
-    
+    Reduces all non branching paths to multiple letter paths
+    Does NOT work so far"""
+    for key, value in list(root_node.items()):
+        if key in ['A', 'C', 'G', 'T', ]:
+            if edge_len(value) >= 2:
+                tree_reducer(value)
+            if edge_len(value) == 1:
+                text = key
+                current = value
+                while edge_len(current) == 1 and '_e' not in current.keys():
+                    current = list(current.values())[0]
+                    text += list(current.keys())[0]
+                del root_node[key]
+                root_node[text] = current
+                tree_reducer(current)
+    return None
+
+def tree_printer(root_node, conn):
+    for key, value in root_node.items():
+        if key == '_e':
+            for index in value:
+                conn.write(genome[index:]+'\n')
+        else:
+            conn.write(key+'\n')
+            tree_printer(value, conn)
+    return None
 
 
 if __name__ == '__main__':
     os.chdir('C:\\Users\\Jacques\\Downloads')
-    f = open('dataset_294_8.txt', 'r')
-    genome = f.readline() + '$'
+    with open('dataset_294_8.txt', 'r') as f:
+        genome = f.readline()
     suffix_tree = suffix_tree_builder(genome)
+    with open('suffix_out.txt', 'w') as g:
+        tree_printer(suffix_tree, g)
+
